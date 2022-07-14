8d469ea59f22558fbc7856a124a1b4d6bc71bb14
Dohyun822 <98006584+Dohyun822@users.noreply.github.com>
2022-07-14 18:00:01 +0900

    프로그래머스 / 같은 숫자는 싫어

---
+++ 12906.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Solution {
+	public int[] solution(int[] arr) {
+		int size = 0;
+
+		Queue<Integer> q = new LinkedList<>();
+		for (int i = 0; i < arr.length; i++) {
+			if (i == 0) {
+				q.offer(arr[i]);
+				size++;
+			} else if (arr[i - 1] == (arr[i])) {
+				continue;
+			} else {
+				q.offer(arr[i]);
+				size++;
+			}
+		}
+		int[] answer = new int[size];
+		for (int i = 0; i < size; i++) {
+			answer[i] = q.poll();
+		}
+
+		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+		System.out.println("Hello Java");
+
+		return answer;
+	}
+}
