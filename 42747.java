0ff6a1136f6a9a260680474b2c3f25ec99258061
Dohyun822 <98006584+Dohyun822@users.noreply.github.com>
2022-07-26 17:59:45 +0900

    프로그래머스 / H-Index

--- 42747.java
+++ 42747.java
@@ -3,18 +3,14 @@ import java.util.*;
 class Solution {
 	public int solution(int[] citations) {
 		int answer = 0;
-		Arrays.sort(citations);
-		int len = citations.length;
-		int x = 0;
-		int y = 0;
-		for (int i = 0; i < len; i++) {
-			x = citations[i];
-			y = len - i;
 
-			if (y <= x) {
-				answer = y;
+		Arrays.sort(citations);
+
+		for (int i = citations.length - 1; i >= 0; i--) {
+			if (citations.length - i >= citations[i]) {
 				break;
 			}
+			answer++;
 		}
 		return answer;
 	}
